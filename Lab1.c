25579108e1fdab2a3740f6da5fad73333d54ebb5
Dwigen <danieljwigen@gmail.com>
2018-01-24 12:16:01 -0800

    Lab one final code
    Final upload for lab 1 code

--- Lab1.c
+++ Lab1.c
@@ -3,114 +3,81 @@
 MILESTONE: 1
 PROGRAM: 1
 PROJECT: MyFirstProject
-GROUP: X
+GROUP: 2
 NAME 1: Daniel, Wigen, V00797593
-NAME 2: Nicola, Watts, V00
-DESC: This program does
+NAME 2: Nicola, Watts, V00822833
+DESC: This program lights up the on board LEDs on the AT90USBKEY2, and creates
+	  the knightrider LED display on the LEDs connected to Port D.
 DATA
 REVISED
 #################################################
 */
 
+//####### Include Files #######
 #include <stdlib.h>
 #include <avr/io.h>
 #include <util/delay_basic.h>
 
+//########### Prototypes ############
+void delaynms(int n);
+void delaynus(int n);
+void knightrider();
+void lightup();
+
+int delayTime = 10; //Delay time between led changes in ms
+
 //################ MAIN ROUTINE ##############
 int main(int argc, char const *argv[]) {
-  DDRD = 0xFF;
-  DDRC = 0xFF;
-  PORTD = 0xA0; //Lab 1 part one. Turn on LEDs
-  PORTC = 0x00;
-  //knightrider(); //lab 1 Part 2, comment out for part one
-  return 0;
+	//Set port D and C pins to output
+	DDRD = 0xFF;
+	DDRC = 0xFF;
+
+	PORTD = 0xA0; //Lab 1 part one. Turn on LEDs
+	PORTC = 0x00;
+	while(1){		//Runs knightrider function infinitely
+		knightrider();
+	}
+	return 0;
 }
 
-void delaynus(int n) {
-  int k;
-  for(k=0; k<n, k++)
-    _delay_loop_1(1);
+//########## Program Functions ##########
+void delaynus(int n) { //function to delay one microsecond
+	int k;
+	for(k=0; k<n; k++){
+		_delay_loop_1(1);
+	}
 }
 
-void delaynms(int n) {
-  int k;
-  for (k = 0; k < n; k++)
-    delaynus(1000);
+void delaynms(int n) { //function to delay one millisecond
+	int k;
+	for (k = 0; k < n; k++){
+		delaynus(1000);
+	}
 }
 
-void out_in() {
-  PORTC = 0x18;
-  delaynms(delayTime);
-  PORTC = 0x24;
-  delaynms(delayTime);
-  PORTC = 0x42;
-  delaynms(delayTime);
-  PORTC = 0x81;
-  delaynms(delayTime);
-  PORTC = 0x42;
-  delaynms(delayTime);
-  PORTC = 0x24;
-  delaynms(delayTime);
-  PORTC = 0x18;
-  delaynms(delayTime);
-  }
-}
-
-void back_forth() {
-  PORTC = 0x01;
-  for (size_t i = 0; i < 7; i++) {
-    delaynms(delayTime)
-    PORTC = PORTC << 1;
-  }
-  for (size_t i = 0; i < 7; i++) {
-    delaynms(delayTime)
-    PORTC = PORTC >> 1;
-  }
-  delaynms(delayTime);
-}
-
-void transfer() {
-  PORTC = 0x04;
-  delaynms(delayTime);
-  PORTC = 0x02;
-  delaynms(delayTime);
-  PORTC = 0x01;
-  delaynms(delayTime);
-}
-
-void knightrider() {
-  delayTime = 50; //Delay time between led changes in ms
-  out_in();
-  out_in();
-  back_forth();
-  back_forth();
-  PORTC = 0x00;
-  PORTD = 0x00;
-}
-for (size_t i = 0; i < count; i++) {
-  /* code */
-}
-void lightup() {
-  PORTD = 0x1C;
-  delaynms(1500);
-  PORTD = 0x10;
-  delaynms(1500);
-  PORTD = 0x20;
-  delaynms(1500);
-  PORTD = 0x31;
-  delaynms(1500);
-  PORTD = 0x40;
-  delaynms(1500);
-  PORTD = 0x19;
-  delaynms(1500);
-  PORTD = 0xAA;
-  delaynms(1500);
-  PORTD = 0x80;
-  delaynms(1500);
-  PORTD = 0x55;
-  delaynms(1500);
-  PORTD = 0xA0;
-  delaynms(1500);
-  PORTD = 0xC0;
-  delaynms(1500);
+void knightrider() {// Creates knightrider pattern on LEDs
+	PORTC = 0x03;
+	delaynms(delayTime);
+	PORTC = 0x07;
+	delaynms(delayTime);
+	PORTC = 0x0F;
+	delaynms(delayTime);
+	for (size_t i = 0; i < 3; i++) { // Left shift LEDs
+		PORTC = PORTC << 1;
+		delaynms(delayTime);
+	}
+	PORTC = 0xE0;
+	delaynms(delayTime);
+	PORTC = 0xC0;
+	delaynms(delayTime);
+	PORTC = 0xE0;
+	delaynms(delayTime);
+	PORTC = 0xF0;
+	delaynms(delayTime);
+	for (size_t i = 0; i < 4; i++) { // Right shift LEDs
+		PORTC = PORTC >> 1;
+		delaynms(delayTime);
+	}
+	PORTC = 0x07;
+	delaynms(delayTime);
 }
